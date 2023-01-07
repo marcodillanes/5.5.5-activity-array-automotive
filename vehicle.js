a5384d0a64e8cf35f665d22a43abb5c7e2773402
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-01-07 10:12:16 -0800

    completed?

--- vehicle.js
+++ vehicle.js
@@ -9,6 +9,7 @@ class Vehicle {
         this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
+        this.fuel = 100
     }
 
     start() {
@@ -52,6 +53,7 @@ class Vehicle {
         }
     }
     stop() {
+        console.log('engine off')
         this.started = false;
     }
 
@@ -76,7 +78,7 @@ class Vehicle {
         if (this.numberOfWheels == 8 && 8 == wheels) {
             console.log(this.model + " " + this.make + " is a Truck");
         } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
+            console.log(this.model + " " + this.make + " is a Car");
         } else if (this.numberOfWheels == 2 && 2 == wheels) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
